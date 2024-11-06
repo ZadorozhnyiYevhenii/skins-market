9610645a3a86a9f9fc5c9b38b7e5dcfb39862baa
ZadorozhnyiYevhenii <yevhenii.zadorozhnyi.dev@gmail.com>
2024-11-06 15:42:03 +0200

    chore: nuxt font module dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  modules: [
+    '@nuxt/fonts',
+  ],
   css: ["~/assets/base.scss"],
   vite: {
     css: {
