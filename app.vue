18fa7a3ca2b5ecee9bea89a3be0a692de309f0be
ZadorozhnyiYevhenii <yevhenii.zadorozhnyi.dev@gmail.com>
2024-11-06 15:24:48 +0200

    chore: nuxt, scss dependencies

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtLoadingIndicator />
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss"></style>
