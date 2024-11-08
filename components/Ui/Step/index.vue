39b6b7f60e1ec01b59505da921c4b669b5a913cd
ZadorozhnyiYevhenii <yevhenii.zadorozhnyi.dev@gmail.com>
2024-11-08 15:30:07 +0200

    feat: add stepper component

---
+++ components/Ui/Step/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { activeStepKey } from "../Stepper/consts/provide-keys";
+
+const { title } = defineProps<{
+  title: string;
+}>();
+
+const activeStep = inject<Ref<string>>(activeStepKey);
+
+const isActive = computed(() => activeStep?.value === title);
+</script>
+
+<template>
+  <article class="step-content" v-show="isActive">
+    <slot />
+  </article>
+</template>
+
+<style scoped lang="scss">
+.step-content {
+  background-color: $thirdly-background-color;
+
+  height: 200px;
+  padding: 12px;
+}
+</style>
---
+++ components/Ui/Stepper/index.vue
@@ -0,0 +1,100 @@
+<script setup lang="ts">
+import { activeStepKey } from "./consts/provide-keys";
+
+defineEmits<{
+  (e: "done"): void;
+}>();
+
+const activeIndex = ref(1);
+
+const slots = useSlots().default;
+
+const steps: string[] = slots ? slots().map((slot) => slot.props?.title) : [];
+
+const activeStep = ref(steps.at(0));
+
+provide(activeStepKey, readonly(activeStep));
+
+const isLastStep = (stepIndex: number) => stepIndex !== steps.length - 1;
+
+const nextStep = () => {
+  if (activeIndex.value <= steps.length) {
+    activeIndex.value++;
+  }
+};
+
+const prevStep = () => {
+  if (activeIndex.value > 1) {
+    activeIndex.value--;
+  }
+};
+
+watch(activeIndex, () => {
+  activeStep.value = steps[activeIndex.value - 1];
+});
+</script>
+
+<template>
+  <section class="stepper">
+    <ul class="stepper__list">
+      <li
+        v-for="(step, index) in steps"
+        :key="index"
+        @click="activeIndex = index + 1"
+        :class="['stepper__item', { active: index + 1 === activeIndex }]"
+      >
+        {{ step }}
+
+        <SvgoArrowNext
+          v-if="isLastStep(index)"
+          class="stepper__arrow-next"
+          alt="Next step"
+        />
+      </li>
+    </ul>
+
+    <slot />
+
+    <slot name="actions" v-bind="{ prevStep, nextStep }" />
+  </section>
+</template>
+
+<style scoped lang="scss">
+.stepper {
+  &__list {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    gap: 9px;
+
+    padding-inline: 20px;
+    padding-bottom: 20px;
+  }
+
+  &__item {
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 12px;
+
+    opacity: 60%;
+
+    display: flex;
+    align-items: center;
+    gap: 9px;
+
+    cursor: pointer;
+
+    text-transform: uppercase;
+
+    &.active {
+      color: #ffb287;
+    }
+  }
+
+  &__arrow-next {
+    width: 16px;
+    height: 16px;
+  }
+}
+</style>
